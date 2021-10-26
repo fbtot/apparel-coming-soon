e7613688319521556fd55a3f6184d9219954643d
Filippo Bristot <filippo.bristot@hotmail.it>
2021-10-26 07:16:23 +0200

    + form validation

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
+const button = document.getElementById("form-button");
+const emailInput = document.getElementById("email");
+const errorMsg = document.getElementById("error-message");
 
+button.addEventListener("click", function (event) {
+  event.preventDefault();
+
+  if (emailInput.validity.valid) {
+    emailInput.classList.remove("error");
+    errorMsg.classList.remove("error");
+    console.log("ok");
+  } else {
+    emailInput.classList.add("error");
+    errorMsg.classList.add("error");
+  }
+});
